27747105d3a41a961bb64fe54a5d6facbec16755
Dennis Duan <gwduan@gmail.com>
2014-08-18 19:35:37 +0800

    Add Redis support with redigo driver.

---
+++ models/myredis/myredis.go
@@ -0,0 +1,51 @@
+package myredis
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+var pool *redis.Pool
+
+func Conn() redis.Conn {
+	return pool.Get()
+}
+
+/*
+func Close() {
+	pool.Close()
+}
+*/
+
+func newPool(server, password string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle: 3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func () (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			if password != "" {
+				 _, err := c.Do("AUTH", password)
+				if err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func init() {
+	server := beego.AppConfig.String("cache::server")
+	password := beego.AppConfig.String("cache::password")
+
+	pool = newPool(server, password)
+}
