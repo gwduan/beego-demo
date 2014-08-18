8341f56ec5c6f866c60f06029da3f296ee02d470
Dennis Duan <gwduan@gmail.com>
2014-08-18 19:33:15 +0800

    Add MongoDB support with mgo driver.

---
+++ models/mymongo/mymongo.go
@@ -0,0 +1,30 @@
+package mymongo
+
+import (
+	"github.com/astaxie/beego"
+	"gopkg.in/mgo.v2"
+)
+
+var session *mgo.Session
+
+func Conn() *mgo.Session {
+	return session.Copy()
+}
+
+/*
+func Close() {
+	session.Close()
+}
+*/
+
+func init() {
+	url := beego.AppConfig.String("mongodb::url")
+
+	sess, err := mgo.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+
+	session = sess
+	session.SetMode(mgo.Monotonic, true)
+}
