ffae779a00c579b0fb850472877d594d9370d9d8
Dennis Duan <gwduan@gmail.com>
2015-04-29 17:13:39 +0800

    Add mysql support.

---
+++ models/mymysql/mymysql.go
@@ -0,0 +1,28 @@
+package mymysql
+
+import (
+	"database/sql"
+	"github.com/astaxie/beego"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var session *sql.DB
+
+func Conn() *sql.DB {
+	return session
+}
+
+func init() {
+	url := beego.AppConfig.String("mysql::url")
+
+	db, err := sql.Open("mysql", url)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	session = db
+}
